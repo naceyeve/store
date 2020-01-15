e6268f49680571a4f1ae02e21889280fc8a28c0a
naceyeve <271948021@qq.com>
2020-01-15 23:01:06 +0800

    接入云

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,96 +1,37 @@
-// miniprogram/pages/home/home.js
+// pages/home/home.js
+const db = wx.cloud.database({
+  env: 'naceyeve'
+})
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    productList: [{
-      id: 1,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
-      name: 'Wallet',
-      price: 100,
-      source: 'CHINA',
-    }, {
-      id: 2,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
-      name: 'Guitar',
-      price: 200,
-      source: 'SWEDEN',
-    }, {
-      id: 3,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product3.jpg',
-      name: 'Stapler',
-      price: 300,
-      source: 'GERMANY',
-    }, {
-      id: 4,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product4.jpg',
-      name: 'Leafy vegetables',
-      price: 400,
-      source: 'NEW ZEALAND',
-    }, {
-      id: 5,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product5.jpg',
-      name: 'Compass',
-      price: 500,
-      source: 'USA',
-    }] // Products List
+    productList: [], // Products List
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad() {
+    this.getProductList()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  getProductList() {
+    wx.showLoading({
+      title: 'Still Loading...',
+    })
 
+    db.collection('product').get().then(result => {
+      wx.hideLoading()
+
+      const productList = result.data
+      // 2 digits for price
+      productList.forEach(product => product.price = parseFloat(Math.round(product.price * 100) / 100).toFixed(2))
+
+      if (productList.length) {
+        this.setData({
+          productList
+        })
+      }
+    }).catch(err => {
+      console.error(err)
+      wx.hideLoading()
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
